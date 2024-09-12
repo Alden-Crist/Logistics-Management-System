b79611b3074d23853377fa227daf34d63f0701c5
Alden-Crist <aldencristrego08@gmail.com>
2024-09-12 23:29:13 +0530

    added config

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+
+//Connection to the Local Database
+const sequelize = new Sequelize('logistic_management', 'root', 'Alden@88', {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false,
+});
+
+
+// sequelize.authenticate()
+//     .then(() => console.log('Database connected.'))
+//     .catch(err => console.error('Unable to connect to the database:', err));
+
+
+
+
+module.exports = sequelize;
