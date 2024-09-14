37c07dd6cc252342c7b1e8d50e43de395fc5e7bb
Alden-Crist <aldencristrego08@gmail.com>
2024-09-14 13:29:57 +0530

    commmit

--- frontend/src/supplier/supplierDashBoard.jsx
+++ frontend/src/supplier/supplierDashBoard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect ,useCallback } from 'react';
 import axios from 'axios';
-import './supplierDashboard.css';
+import './supplierDashboard1.css';
 
 
 const SupplierDashboard = () => {
